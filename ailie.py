7fe699828c64cb75241eb10a79b67b9312fb49ad
riazufila <46749569+riazufila@users.noreply.github.com>
2021-03-16 20:58:44 +0800

    Moved from main to cog

--- ailie.py
+++ ailie.py
@@ -2,7 +2,6 @@
 
 import os
 import asyncio
-import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -17,37 +16,10 @@ if __name__ == "__main__":
     bot.remove_command("help")
 
     # Load extensions
-    extensions = ["cogs.help", "cogs.hero", "cogs.equipment"]
+    extensions = ["cogs.basic", "cogs.help", "cogs.hero", "cogs.equipment"]
 
     for extension in extensions:
         bot.load_extension(extension)
-
-    # Bot event on_ready
-    @bot.event
-    async def on_ready():
-        print(f"{bot.user} is collecting!")
-        await bot.change_presence(activity=discord.Game("Guardian Tales"))
-
-    @bot.command(name="version", help="Shows Ailie's current version.")
-    @commands.cooldown(1, 15, commands.BucketType.user)
-    async def version(ctx):
-        version = "1.0.1"
-        msg = await ctx.send(f"Hello, <@{ctx.author.id}>! Ailie reporting to duty!")
-        await asyncio.sleep(1.5)
-        await msg.edit(content=msg.content + "\nMy current version is")
-        await asyncio.sleep(0.5)
-        await msg.edit(content=msg.content + ".")
-        await asyncio.sleep(0.5)
-        await msg.edit(content=msg.content + ".")
-        await asyncio.sleep(0.5)
-        await msg.edit(content=msg.content + f" {version}!")
-        await asyncio.sleep(0.5)
-
-    # On cooldown
-    @bot.event
-    async def on_command_error(ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            await ctx.send(f"Hey, <@{ctx.author.id}>.. {error}!")
 
     # Run bot
     bot.run(TOKEN)
---
+++ cogs/basic.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import asyncio
+import discord
+from discord.ext import commands
+
+
+class Basic(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Execute when bot is ready
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{self.bot.user} is collecting!")
+        await self.bot.change_presence(activity=discord.Game("Guardian Tales"))
+
+    # Send error message upon spamming commands
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.send(f"Hey, <@{ctx.author.id}>.. {error}!")
+
+    # Retrieve Ailie's version
+    @commands.command(name="version", help="Shows Ailie's current version.")
+    async def version(self, ctx):
+        # Change upon version update
+        version = "1.0.1"
+
+        # Mimic loading animation
+        msg = await ctx.send(f"Hello, <@{ctx.author.id}>! Ailie reporting to duty!")
+        await asyncio.sleep(1.5)
+        await msg.edit(content=msg.content + "\nMy current version is")
+        await asyncio.sleep(0.5)
+        await msg.edit(content=msg.content + ".")
+        await asyncio.sleep(0.5)
+        await msg.edit(content=msg.content + ".")
+        await asyncio.sleep(0.5)
+        await msg.edit(content=msg.content + f" {version}!")
+        await asyncio.sleep(0.5)
+
+
+def setup(bot):
+    bot.add_cog(Basic(bot))
