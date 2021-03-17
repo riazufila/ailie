cf827e831650937a2a6df5a11599d5747d8071c6
riazufila <46749569+riazufila@users.noreply.github.com>
2021-03-17 19:50:15 +0800

    Latency command

--- cogs/basic.py
+++ cogs/basic.py
@@ -15,14 +15,14 @@ class Basic(commands.Cog):
         print(f"{self.bot.user} is collecting!")
         await self.bot.change_presence(activity=discord.Game("Guardian Tales"))
 
-    # Send error message upon spamming commands
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            await ctx.send(f"Hey, <@{ctx.author.id}>.. {error}!")
+    # Check bot's latency
+    @commands.command(name="ping", help="Check bot's latency.", aliases=["p"])
+    @commands.cooldown(1, 45, commands.BucketType.user)
+    async def ping(self, ctx):
+        await ctx.send(f"Pong, <@{ctx.author.id}>! Sending back this message with {round(self.bot.latency * 1000)}ms latency.")
 
     # Retrieve Ailie's version
-    @commands.command(name="version", help="Shows Ailie's current version.")
+    @commands.command(name="version", help="Shows Ailie's current version.", aliases=["v"])
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def version(self, ctx):
         # Change upon version update
@@ -39,6 +39,12 @@ class Basic(commands.Cog):
         await asyncio.sleep(0.5)
         await msg.edit(content=msg.content + f" {version}!")
         await asyncio.sleep(0.5)
+
+    # Send error message upon spamming commands
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.send(f"Hey, <@{ctx.author.id}>.. {error}!")
 
 
 def setup(bot):
