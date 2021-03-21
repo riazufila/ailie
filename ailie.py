9477a4303cb3819b7016229f0a7191bab7bb8c1e
riazufila <46749569+riazufila@users.noreply.github.com>
2021-03-21 15:35:49 +0800

    Delete db variables

--- ailie.py
+++ ailie.py
@@ -9,11 +9,6 @@ if __name__ == "__main__":
     # Get secrets
     load_dotenv()
     TOKEN = os.getenv("DISCORD_TOKEN")
-    DB_NAME = os.getenv("DB_NAME")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT")
 
     # Bot setup
     bot = commands.Bot(
