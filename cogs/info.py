fb2431f2ac8596aceaeb9204deae1fac0b21a6e9
riazufila <46749569+riazufila@users.noreply.github.com>
2021-03-22 17:26:54 +0800

    Updated version info

--- cogs/info.py
+++ cogs/info.py
@@ -36,7 +36,7 @@ class Info(commands.Cog):
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def version(self, ctx):
         # Change upon version update
-        version = "1.1.3"
+        version = "1.2.0"
 
         # Mimic loading animation
         msg = await ctx.send(
