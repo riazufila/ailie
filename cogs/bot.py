1e9a3ece249f516b02f812e0a3d4ae0838614174
riazufila <46749569+riazufila@users.noreply.github.com>
2021-05-07 19:01:41 +0800

    Updated version info

--- cogs/bot.py
+++ cogs/bot.py
@@ -91,7 +91,7 @@ class Bot(commands.Cog):
         db_ailie.disconnect()
 
         # Change upon version update
-        version = "1.4.1"
+        version = "1.4.2"
 
         # Mimic loading animation
         msg = await ctx.send(
